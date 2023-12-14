ca490bf7957d2e0b407a60d0c2d4a476a7a46441
josue-flores03 <josue.flores03@utrgv.edu>
2023-12-14 14:00:42 -0600

    Functional in c++

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 gcc syllable_checking.c -o syllable_checking
 ./syllable_checking
 
+g++ -o syllable_checking_2 syllable_checking.cpp
+./syllable_checking_2
+
 
 
 q = sh
--- looping.c
+++ looping.c
@@ -16,35 +16,35 @@ int main (int argc, char **argv)
     while (fgets(line, sizeof(line), text_file))
     {
         if (i <= 75)
-            line[3] = 'm';//printf("m\n");
+            line[3] = 'm';
         else if (i <= 150)
-            line[3] = 'n';//printf("n\n");
+            line[3] = 'n';
         else if (i <= 225)
-            line[3] = 'p';//printf("p\n");
+            line[3] = 'p';
         else if (i <= 300)
-            line[3] = 'f';//printf("f\n");
+            line[3] = 'f';
         else if (i <= 375)
-            line[3] = 't';//printf("t\n");
+            line[3] = 't';
         else if (i <= 450)
-            line[3] = 'd';//printf("d\n");
+            line[3] = 'd';
         else if (i <= 525)
-            line[3] = 's';//printf("s\n");
+            line[3] = 's';
         else if (i <= 600)
-            line[3] = 'q';//printf("q\n");
+            line[3] = 'q';
         else if (i <= 675)
-            line[3] = 'r';//printf("r\n");
+            line[3] = 'r';
         else if (i <= 750)
-            line[3] = 'l';//printf("l\n");
+            line[3] = 'l';
         else if (i <= 825)
-            line[3] = 'y';//printf("y\n");
+            line[3] = 'y';
         else if (i <= 900)
-            line[3] = 'k';//printf("k\n");
+            line[3] = 'k';
         else if (i <= 975)
-            line[3] = 'g';//printf("g\n");
+            line[3] = 'g';
         else if (i <= 1050)
-            line[3] = 'h';//printf("h\n");
+            line[3] = 'h';
         else if (i <= 1125)
-            line[3] = 'j';//printf("j\n");
+            line[3] = 'j';
         
         printf("%s\n", line);
         fprintf(output_file, "%s\n", line);
